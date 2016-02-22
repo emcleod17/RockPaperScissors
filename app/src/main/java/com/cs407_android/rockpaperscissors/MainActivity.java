d9f75d5505b3fdb2b048915f8b301db31b06edaa
emcleod17 <emcleod@wisc.edu>
2016-02-22 15:39:58 -0600

    -tutorial 3 stuff, added a menu bar

--- app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/cs407_android/rockpaperscissors/MainActivity.java
@@ -3,6 +3,7 @@ package com.cs407_android.rockpaperscissors;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         if (savedInstanceState != null) {
             return;
